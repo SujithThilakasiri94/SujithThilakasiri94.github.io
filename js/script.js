2df0962a712b59fad6c2d0e744e4d4b653bcf932
SujithThilakasiri94 <sujiththilaksiri@outlook.com>
2022-11-24 14:18:56 +0530

    code refignment,remove ! from html

--- js/script.js
+++ js/script.js
@@ -1,63 +1,73 @@
 "use strict";
 
 let highScore = 0,
-    mark,
-    hiddenNum;
+    mark = 20,
+    hiddenNum,
+    playerGuessed;
 
+//function to generate random number
 function hiddenNumGenerator() {
     hiddenNum = Math.trunc(Math.random() * 20 + 1);
     document.querySelector("body").style.backgroundColor = "#101111";
-    document.querySelector(".guess").value = "";
-    //console.log(hiddenNum);
+    valueAssigner(".guess", "");
 }
+//function to display text in dom
+const textAssigner = (element, text) =>
+    (document.querySelector(element).textContent = text);
+
+//function to display numbers in dom
+const valueAssigner = (element, value) =>
+    (document.querySelector(element).value = value);
 
 hiddenNumGenerator();
 
+//if clicked on check button
 document.querySelector(".check").addEventListener("click", () => {
+    playerGuessed = Number(document.querySelector(".guess").value);
+    valueAssigner(".guess", "");
 
-    const guess = Number(document.querySelector(".guess").value);
-    document.querySelector(".guess").value = "";
+    // if not a number
+    if (!playerGuessed) {
+        textAssigner(".message", "Not a Number 🛑");
+    }
+    //if the guessed number is wrong
+    else if (Number(playerGuessed) < hiddenNum) {
+        textAssigner(".message", "Your Number too low ⬇");
+        mark = mark - 1;
+        textAssigner(".score", mark);
+    } else if (Number(playerGuessed) > hiddenNum) {
+        textAssigner(".message", "Your Number too high ⬆");
+        mark = mark - 1;
+        textAssigner(".score", mark);
+    }
+    else {
+        textAssigner(".score", mark);
+        textAssigner(".message", `👏You guessed it. Hidden number is ${hiddenNum}`);
+        document.querySelector("body").style.backgroundColor = "#33c26f";
+        valueAssigner(".guess", playerGuessed);
 
-    if (!guess) {
-        document.querySelector(".message").textContent = "Not a Number 🛑";
-
-    } else {
-        if (hiddenNum != Number(guess)) {
-            mark = document.querySelector(".score").textContent - 1;
-            document.querySelector(".score").textContent = mark;
-            if (Number(guess) < hiddenNum) {
-                document.querySelector(".message").textContent = "Your Number too low";
-            } else {
-                document.querySelector(".message").textContent = "Your Number too high";
-            }
-            if (mark < 1) {
-                mark = 0;
-                document.querySelector(".message").textContent =
-                    `Hidden Number was ${hiddenNum}.
-                    Your score is 0.Click 'Again' button`;
-                document.querySelector(".score").textContent = mark;
-                document.querySelector("body").style.backgroundColor = "#da262f";
-            }
-        } else {
-            document.querySelector(".score").textContent = mark;
-            console.log("Mark = " + mark);
-            document.querySelector(
-                ".message"
-            ).textContent = `You guessed it. Hidden number is ${hiddenNum}`;
-            document.querySelector("body").style.backgroundColor = "#33c26f";
-            document.querySelector(".guess").value = guess;
-
-            if (mark > highScore) {
-                highScore = Number(mark);
-                document.querySelector(".highScore").textContent = highScore;
-            }
+        if (mark > highScore) {
+            highScore = Number(mark);
+            document.querySelector(".highScore").textContent = highScore;
         }
     }
+    if (mark < 1) {
+        mark = 0;
+        textAssigner(
+            ".message",
+            `Hidden Number was ${hiddenNum}.
+                Your score is 0.Click 'Again' button`
+        );
+        document.querySelector("body").style.backgroundColor = "#da262f";
+        //if the guessed number is correct
+    }
+
 });
 
+//if clicked on restart button
 document.querySelector(".again").addEventListener("click", function () {
     hiddenNumGenerator();
     mark = 20;
-    document.querySelector(".score").textContent = mark;
-    document.querySelector(".message").textContent = "Start guessing...";
+    textAssigner(".score", mark);
+    textAssigner(".message", "Start guessing...");
 });
